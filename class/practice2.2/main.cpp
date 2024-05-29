c327ccaf53d4ea93ec380c9134acafa77614c334
DavidBakalov21 <davidbakalov33@gmail.com>
2024-05-29 22:11:18 +0000

    class 2.2 fixed

--- class/practice2.2/main.cpp
+++ class/practice2.2/main.cpp
@@ -5,171 +5,200 @@
 #include <sstream>
 #include <ranges>
 #include <print>
+#include <cstdint>
+
+namespace {
+   constexpr const int MatrixSize = 16;
+}
 
 std::vector<std::string> splitString(const std::string& str, const char delimiter) {
     return str  | std::ranges::views::split(delimiter)
        | std::ranges::to<std::vector<std::string>>();
 }
 
-class Pixel
-{
-private:
-    std::string color;
-    int r;
-    int g;
-    int b;
+struct Color {
+    int r{ -1 };
+    int g{ -1 };
+    int b{ -1 };
+    };
+
+class Pixel{
 public:
-    Pixel(const std::string& col):color(col){}
-    std::string getColor() const {
+    Pixel() = default;
+    Pixel(const std::string& col){
+       auto values = splitString(col, ',');
+       if (values.size() == 3) {
+        color.r=convertToInt(values[0]);
+        color.g=convertToInt(values[1]);
+        color.b=convertToInt(values[2]);
+       }
+        
+    }
+
+    Color getColor() const {
         return color;
     }
 
-
- bool isValid() const {
-    return (red >= 0 && red <= 255) &&
-            (green >= 0 && green <= 255) &&
-            (blue >= 0 && blue <= 255);
+    std::string serToString() const{
+        std::ostringstream oss;
+        oss << (int)color.r<<","
+        <<(int)color.g<<","
+        <<(int)color.b;
+        return oss.str();
     }
 
-
-
-    void setColor(const std::string& col){
-        color=col;
+    bool isValid() const {
+        return (color.r >= 0 && color.r <= 255) &&
+            (color.g >= 0 && color.g <= 255) &&
+            (color.b >= 0 && color.b <= 255);
     }
 
-    bool isValid(){
-         std::vector<std::string> elements = splitString(color, ',');
-        if (elements.size() < 3) {
-            return false;
+    bool compareColor(const Pixel& compare) const {
+        return (compare.color.b==color.b) && (compare.color.g==color.g) && (compare.color.r==color.r);
+    }
+
+    void setColor(const Pixel& col){
+         color = col.getColor();
+    }
+
+private:
+
+    int convertToInt(const std::string& str) {
+        try {
+            return static_cast<int>(std::stoi(str));
+        } catch (const std::invalid_argument& ia) {
+            return -1;
+        } catch (const std::out_of_range& oor) {
+            return -1;
         }
-        for (int i = 0; i < elements.size(); i++)
-        {
-            for (int j = 0; j < elements[i].size(); j++) {
-                if (std::isalpha(static_cast<unsigned char>(elements[i][j]))) {
-                    return false;
-                }
-            }
-            int num = std::atoi(elements[i].c_str());
-            if (num < 0 || num >255) {
-                return false;
-            }
-        }
-        return true;
     }
+    Color color;
 };
 
 std::string listToStr(const std::vector<Pixel>& list) {
     std::ostringstream oss;
-    for (const Pixel& str : list) {
+
+    for (const Pixel& pix : list) {
         if (!oss.str().empty()) {
             oss << " ";
         }
-        oss << str.getColor();
+        oss << pix.serToString();
     }
     return oss.str();
-}
-
-namespace {
-    const int MatrixSize = 16;
 }
 
 class Image {
 private:
     std::vector<std::vector<Pixel>> lines;
-    std::string file;
-    Pixel color;
+    std::string pixelMatrixFile;
+
+public:
+
+    Image(const std::string& filePath) : pixelMatrixFile(filePath), lines(MatrixSize, std::vector<Pixel>(MatrixSize)) {}
+
+    int loadMatrix() {
+        std::string line;
+        std::ifstream pixelMatrix(pixelMatrixFile);
+        int indexCounter=0;
+        while (std::getline(pixelMatrix, line)) {
+            std::vector<std::string>  splittedLine = splitString(line, ' ');
+            std::vector<Pixel> pixels(MatrixSize);
+             if (splittedLine.size() != MatrixSize)
+            {
+                return 1;
+            }
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                Pixel pix(splittedLine[i]);
+                if (!pix.isValid()) {
+                    return 2;
+                }
+                pixels[i]=pix;
+            }
+
+            if (indexCounter==MatrixSize)
+            {
+                return 1;
+            }
+            
+            lines[indexCounter]=pixels;
+            indexCounter++;
+        }
+        pixelMatrix.close();
+        return 0;
+    }
+ 
+    void changeMatrix(const Pixel& favouriteColor) {
+        for (int i = 0; i < MatrixSize; i++)
+        {
+            for (int j = 0; j < MatrixSize; j++)
+            {
+                if (lines[i][j].compareColor(favouriteColor)) {
+                    if (i > 0)
+                    {
+                        lines[i - 1][j].setColor(favouriteColor);
+                    }
+                    if (j > 0)
+                    {
+                        lines[i][j - 1].setColor(favouriteColor);
+                    }
+                }
+            }
+        }
+    }
 
     int saveFile() {
-        std::ofstream fileEdit("Output"+file, std::ios::trunc);
-        for (const auto& line : lines) {
-            std::string convertedList=listToStr(line);
-            if (convertedList != "")
-            {
-                fileEdit << convertedList << std::endl;
-            }
+      std::ofstream fileEdit("Output" + pixelMatrixFile, std::ios::trunc);
+      for (int i = 0; i < MatrixSize; i++)
+      {
+          std::string convertedList = listToStr(lines[i]);
+          if (convertedList != "")
+          {
+              fileEdit << convertedList;
+              if (i < MatrixSize - 1) { 
+                  fileEdit << std::endl;
+              }
+          }
         }
         fileEdit.close();
         return 0;
     }
-
-public:
-     Image(const std::string& filePath, const Pixel& colorArgument) : file(filePath), color(colorArgument) {}
-    std::vector<std::vector<Pixel>> getLines() const {
-        return lines;
-    }
-
-    std::string loadMatrix() {
-        std::string line;
-        std::ifstream pixelMatrix(file);
-        while (std::getline(pixelMatrix, line)) {
-            std::vector<std::string>  splittedLine = splitString(line, ' ');
-            std::vector<Pixel> pixels;
-             if (splittedLine.size() != MatrixSize)
-            {
-                return "Please, provide 16x16 image";
-            }
-            for (int i = 0; i < splittedLine.size(); i++)
-            {
-                Pixel pix(splittedLine[i]);
-                if (!pix.isValid()) {
-                    return "Please, provide image with valid colors";
-                }
-                pixels.push_back(pix);
-            }
-            lines.push_back(pixels);
-        }
-        pixelMatrix.close();
-        return "success";
-    }
- 
-    int changeMatrix() {
-        for (int i = 0; i < lines.size(); i++)
-        {
-           
-            for (int j = 0; j < lines.size(); j++)
-            {
-                if (lines[i][j].getColor() == color.getColor()) {
-                    if (i - 1 >= 0)
-                    {
-                        lines[i - 1][j].setColor(color.getColor());
-                    }
-                    if (j - 1 >= 0)
-                    {
-                        lines[i][j - 1].setColor(color.getColor());
-                    }
-                }
-            }
-        }
-        return saveFile();
-    }
-  
 };
+
 int main(int argc, char* argv[]) {
     if (argc < 3)
     {
-        std::cout << "Error:Please provide all arguments" << std::endl;
-        return 0;
+        std::println("Error:Please provide all arguments");
+        return 1;
     }
     
     std::string filename = argv[1];
     std::string colrArgument=argv[2];
     Pixel colorPicked(colrArgument);
-    Image image(filename,colorPicked);
+    Image image(filename);
     if (!colorPicked.isValid()) {
-        std::cout << "Please, enter existing color for example 255,97,105" << std::endl;
-        return 0;
+        std::println("Please, enter existing color for example 255,97,105");
+        return 1;
     }
-    std::string loadResult=image.loadMatrix();
-    if (loadResult != "success") {
-        std::cout << loadResult << std::endl;
-        return 0;
+    
+    int loadResult=image.loadMatrix();
+    if (loadResult != 0) {
+        switch(loadResult) {
+            case 1:
+                std::println("Please, provide 16x16 image");
+                break;
+            case 2:
+                std::println("Please, provide image with valid colors");
+                break;
+            default:
+                std::println("Unknown error happened");
+                break;
+
+        }
+        return 1;
     }
 
-    if (image.getLines().size() != MatrixSize)
-    {
-        std::println("Please, provide {}x{} image", MatrixSize, MatrixSize);
-        return 0;
-    }
-    image.changeMatrix();
-    std::cout << "Image was changed successfully" << std::endl;
+    image.changeMatrix(colorPicked);
+    image.saveFile();
+    std::println("Image was changed successfully");
 }
