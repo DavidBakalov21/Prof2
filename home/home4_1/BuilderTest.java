27ddbea0dd58f5f6aab6cbb92b7734c06f810865
DavidBakalov21 <davidbakalov33@gmail.com>
2024-06-19 18:11:04 +0000

    more  fixes for home 4.1

--- home/home4_1/BuilderTest.java
+++ home/home4_1/BuilderTest.java
@@ -1,50 +1,52 @@
-package home4_1;
-import org.junit.jupiter.api.Test;  
-import static org.junit.jupiter.api.Assertions.*; 
-
-public class BuilderTest {  
-  
-    @Test  
-    public void tenDollarPizza() {  
+package home4_1; 
+import org.junit.jupiter.api.Test;   
+import static org.junit.jupiter.api.Assertions.*;  
+import java.util.AbstractMap.SimpleEntry; 
+import java.util.ArrayList; 
+import java.util.List; 
+public class BuilderTest {   
    
-        Director director = new Director();
-        BestPizzaBuilder bestPizzaBuilder = new BestPizzaBuilder();
-        director.setBuilder(bestPizzaBuilder);
-        List<SimpleEntry<String, Double>> ingredients = new ArrayList<>();
-        ingredients.add(new SimpleEntry<>("mayo", 1.0));
-        ingredients.add(new SimpleEntry<>("meat", 2.0));
-        ingredients.add(new SimpleEntry<>("mozzarella", 3.0));
-        ingredients.add(new SimpleEntry<>("onions", 4.0));
-        director.constructPizza(ingredients);
-        Pizza gamingPizza = bestPizzaBuilder.getPizza()
-
-        // Operate:
-        String result = gamingPizza.showPizza();
-
-        // Check:
-        assertEquals("First: mayo Second: meat Third: mozzarella Fourth: onions Price: $10,00", result);
-    }  
-
-    @Test  
-    public void zeroDollarPizza() {  
-
-
-        Director director = new Director();
-        BestPizzaBuilder bestPizzaBuilder = new BestPizzaBuilder();
-        director.setBuilder(bestPizzaBuilder);
-        List<SimpleEntry<String, Double>> ingredients = new ArrayList<>();
-        ingredients.add(new SimpleEntry<>("nothing", 0));
-        ingredients.add(new SimpleEntry<>("nothing", 0));
-        ingredients.add(new SimpleEntry<>("nothing", 0));
-        ingredients.add(new SimpleEntry<>("nothing", 0));
-        director.constructPizza(ingredients);
-        Pizza gamingPizza = bestPizzaBuilder.getPizza()
-
-        // Operate:
-        String result = gamingPizza.showPizza();
-
-        // Check:
-        assertEquals("First: nothing Second: nothing Third: nothing Fourth: nothing Price: $0,00", result);
-    }
-
+    @Test   
+    public void tenDollarPizza() {   
+    
+        Director director = new Director(); 
+        BestPizzaBuilder bestPizzaBuilder = new BestPizzaBuilder(); 
+        director.setBuilder(bestPizzaBuilder); 
+        List<SimpleEntry<String, Double>> ingredients = new ArrayList<>(); 
+        ingredients.add(new SimpleEntry<>("mayo", 1.0)); 
+        ingredients.add(new SimpleEntry<>("meat", 2.0)); 
+        ingredients.add(new SimpleEntry<>("mozzarella", 3.0)); 
+        ingredients.add(new SimpleEntry<>("onions", 4.0)); 
+        director.constructPizza(ingredients); 
+        Pizza gamingPizza = bestPizzaBuilder.getPizza(); 
+ 
+        // Operate: 
+        String result = gamingPizza.showPizza(); 
+ 
+        // Check: 
+        assertEquals("First: mayo Second: meat Third: mozzarella Fourth: onions Price: $10,00", result); 
+    }   
+ 
+    @Test   
+    public void zeroDollarPizza() {   
+ 
+ 
+        Director director = new Director(); 
+        BestPizzaBuilder bestPizzaBuilder = new BestPizzaBuilder(); 
+        director.setBuilder(bestPizzaBuilder); 
+        List<SimpleEntry<String, Double>> ingredients = new ArrayList<>(); 
+        ingredients.add(new SimpleEntry<>("nothing", 0.0)); 
+        ingredients.add(new SimpleEntry<>("nothing", 0.0)); 
+        ingredients.add(new SimpleEntry<>("nothing", 0.0)); 
+        ingredients.add(new SimpleEntry<>("nothing", 0.0)); 
+        director.constructPizza(ingredients); 
+        Pizza gamingPizza = bestPizzaBuilder.getPizza(); 
+ 
+        // Operate: 
+        String result = gamingPizza.showPizza(); 
+ 
+        // Check: 
+        assertEquals("First: nothing Second: nothing Third: nothing Fourth: nothing Price: $0,00", result); 
+    } 
+ 
 }
