6beb899ba867d15dfe3e2794f813195b2fc01140
DavidBakalov21 <davidbakalov33@gmail.com>
2024-06-30 09:55:02 +0000

    7.1 class

---
+++ class/practice7.1/src/maybeTest.cpp
@@ -0,0 +1,31 @@
+#include <UnitTests.hpp>
+#include <Maybe.hpp>
+int main() {
+    UnitTests testSuite;
+
+    testSuite.addTest("Get area",
+    [](){
+        Rect rect(3.0, 7.0);
+        ASSERT_EQ(rect.getArea(), 21.0)
+    });
+
+    testSuite.addTest("Get biggest side",
+    [](){
+        Rect rect(3.0, 7.0);
+        ASSERT_EQ(rect.getBiggestSide(), 7.0)
+    });
+    testSuite.addTest("Test fits",
+    [](){
+        Rect rect(3.0, 7.0);
+        Rect rect2(1.0, 2.0);
+        ASSERT_EQ(rect2.fitsInside(rect), true)
+    });
+    testSuite.addTest("Smallest area test",
+    [](){
+        RectCollection collection;
+        collection.addRectangle(Rect(5.0, 4.0));
+        collection.addRectangle(Rect(5.0, 6.0));
+        ASSERT_EQ(collection.getSmallestAreaRectangle().getArea(), 20.0)
+    });
+    testSuite.run();
+}
