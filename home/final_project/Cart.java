e94b75df13c92b4a2687f909bb40cad45bf6e2e6
DavidBakalov21 <davidbakalov33@gmail.com>
2024-07-11 17:26:21 +0000

    final project start

---
+++ home/final_project/Cart.java
@@ -0,0 +1,48 @@
+package final_project;
+
+import java.util.ArrayList;
+
+public class Cart{
+ private ArrayList<Product> cart;
+
+    public Cart() {
+        this.cart = new ArrayList<>();
+    }
+
+    // Method to clear the cart
+    public void clearCart() {
+        cart.clear();
+        System.out.println("Cart has been cleared.");
+    }
+
+    public void addToCart(Product product) {
+        cart.add(product);
+        System.out.println(product.getName() + " has been added to the cart.");
+    }
+
+    public void removeFromCart(String prodName) {
+        boolean removed = false;
+        for (Product product : cart) {
+            if (product.getName().equalsIgnoreCase(prodName)) {
+                cart.remove(product);
+                System.out.println(product.getName() + " has been removed from the cart.");
+                removed = true;
+                break;
+            }
+        }
+        if (!removed) {
+            System.out.println("Product not found. No product removed.");
+        }
+    }
+
+    public void viewCart() {
+        if (cart.isEmpty()) {
+            System.out.println("The cart is empty.");
+        } else {
+            System.out.println("Cart contains:");
+            for (Product product : cart) {
+                System.out.println(product.getName() + " - $" + product.getPrice());
+            }
+        }
+    }
+}
---
+++ home/final_project/Menu.java
@@ -0,0 +1,36 @@
+package final_project;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+public class Menu{
+    private ArrayList<Product> menu;
+
+     public void viewMenu() {
+        if (menu.isEmpty()) {
+            System.out.println("The menu is empty.");
+        } else {
+            System.out.println("Menu:");
+            for (Product product : menu) {
+                System.out.println(product.getName() + " - $" + product.getPrice());
+            }
+        }
+    }
+    public void viewMenuPriceSorted() {
+        if (menu.isEmpty()) {
+            System.out.println("The menu is empty.");
+        } else {
+            Collections.sort(menu, new Comparator<Product>() {
+                @Override
+                public int compare(Product p1, Product p2) {
+                    return Double.compare(p2.getPrice(), p1.getPrice());
+                }
+            });
+
+            System.out.println("Menu:");
+            for (Product product : menu) {
+                System.out.println(product.getName() + " - $" + product.getPrice());
+            }
+        }
+    }
+
+}
